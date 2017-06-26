46d98337b657572784e3041f165e58a86e2a6b82
zfq <1586687169@qq.com>
2017-06-26 23:22:26 +0800

    完善

--- js/index.js
+++ js/index.js
@@ -1,12 +1,27 @@
 
 
 
-// window.onload = function() {
-//     $().getClass('member').hover(function(){
-//         $().getByTagName('ul').show();
-//     },function() {
-//         $().getByTagName('ul').hide();;
-//     });
+window.onload = function() {
+    // $().getClass('member').hover(function(){
+    //     $().getByTagName('ul').show();
+    // },function() {
+    //     $().getByTagName('ul').hide();;
+    // });
 
-// }
+
+	$().getClass('login').click(function(){
+		var clientWidth = document.documentElement.clientWidth;
+		var clientHeight = document.documentElement.clientHeight;
+		var ele = $().getById('login_alert').elements[0];
+		var top = (clientHeight - ele.offsetHeight) / 2;
+		var left = (clientWidth - ele.offsetWidth) / 2;
+
+		// ele.style[top] = top + 'px';
+		// ele.style[left] = left + 'px';
+		ele.style[top] = '100px';
+		ele.style[left] = '100px';
+		// $().getById(login_alert).css('top','100px').css('left','200px');
+	});
+
+}
 
